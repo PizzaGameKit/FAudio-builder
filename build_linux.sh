3d7ff37270d9705a918d400f861e1d1383598a41
Thomas Altenburger <3427451+ThomasFOG@users.noreply.github.com>
2025-12-26 16:32:49 +0100

    First dab at building FAudio

---
+++ build_linux.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Move to script's directory
+cd "`dirname "$0"`"
+
+arch=$1
+
+sdlPath="$(cd "./SDL" && pwd -P)"
+faudioPath="$(cd "./FAudio" && pwd -P)"
+
+outputFolder="./binaries/linux-$arch"
+rm -r -f $outputFolder
+mkdir -p $outputFolder
+
+logFolder="./logs/linux-$arch"
+rm -r -f $logFolder
+mkdir -p $logFolder
+
+buildFolder="build"
+
+sdlBuild="$sdlPath/$buildFolder"
+faudioBuild="$faudioPath/$buildFolder"
+
+# Generate SDL
+echo "Generate SDL"
+
+rm -r -f $sdlBuild
+
+cmake -S $sdlPath -B $sdlBuild > "$logFolder/SDL.gen.log"
+
+echo -e "\tDone"
+
+# Build SDL
+echo "Build SDL"
+
+cmake --build $sdlBuild --target all > "$logFolder/SDL.bin.log"
+
+echo -e "\tDone"
+
+# Generate FAudio
+echo "Generate FAudio"
+
+rm -r -f $faudioBuild
+
+cmake -S $faudioPath -B $faudioBuild -DSDL3_DIR="$sdlBuild" > "$logFolder/FAudio.gen.log"
+
+echo -e "\tDone"
+
+# Build FAudio
+echo "Build FAudio"
+
+cmake --build $faudioBuild --target FAudio-shared > "$logFolder/FAudio.bin.log"
+
+cp -f "$faudioBuild/libFAudio.so" "$outputFolder/libFAudio.so"
+
+echo -e "\tDone"
---
+++ build_macos.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Move to script's directory
+cd "`dirname "$0"`"
+
+sdlPath="$(cd "./SDL" && pwd -P)"
+faudioPath="$(cd "./FAudio" && pwd -P)"
+
+outputFolder="./binaries/osx"
+rm -r -f $outputFolder
+mkdir -p $outputFolder
+
+logFolder="./logs/osx"
+rm -r -f $logFolder
+mkdir -p $logFolder
+
+buildFolder="build"
+
+sdlBuild="$sdlPath/$buildFolder"
+faudioBuild="$faudioPath/$buildFolder"
+
+# Generate SDL
+echo "Generate SDL"
+
+rm -r -f $sdlBuild
+
+cmake -S $sdlPath -B $sdlBuild > "$logFolder/SDL.gen.log"
+
+echo -e "\tDone"
+
+# Build SDL
+echo "Build SDL"
+
+cmake --build $sdlBuild --target all > "$logFolder/SDL.bin.log"
+
+echo -e "\tDone"
+
+# Generate FAudio
+echo "Generate FAudio"
+
+rm -r -f $faudioBuild
+
+cmake -S $faudioPath -B $faudioBuild -DSDL3_DIR="$sdlBuild" -DCMAKE_OSX_DEPLOYMENT_TARGET="10.15" -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" > "$logFolder/FAudio.gen.log"
+
+echo -e "\tDone"
+
+# Build FAudio
+echo "Build FAudio"
+
+cmake --build $faudioBuild --target FAudio-shared > "$logFolder/FAudio.bin.log"
+
+cp -f "$faudioBuild/libFAudio.dylib" "$outputFolder/libFAudio.dylib"
+
+echo -e "\tDone"
